10c67dd52e6f8ad25bd0884c17c6832c7ec9098e
nikolay <n.koshelev@tightvideo.com>
2026-06-23 11:44:25 +1000

    add script for the build project

---
+++ build/build.sh
@@ -0,0 +1,28 @@
+#!/bin/env bash
+
+NAMEAPP="app";
+SOURCE_FOLDER="../source";
+SOURCE_FILE="main.c deepMem.c";
+FLAGS="-g -Wall -o ${NAMEAPP}";
+
+
+[[ -n $(ls "${SOURCE_FOLDER}" 2> /dev/null) ]] && {
+  printf "Build...\n";
+  
+  FULL_SOURCE_PATH="";
+  for file_name in ${SOURCE_FILE}
+  do
+    FULL_SOURCE_PATH="${FULL_SOURCE_PATH} ${SOURCE_FOLDER}/${file_name}";
+  done
+  
+  # printf "%s\n" "${FULL_SOURCE_PATH}";
+
+  BUILD="gcc${FULL_SOURCE_PATH} ${FLAGS}"
+  printf "\033[34m%s\033[0m\n" "${BUILD}";
+  
+  echo "${BUILD}" | bash;
+
+  printf "\033[32;40mThe assembly is ready. Your file \"${NAMEAPP}\".\033[0m\n";
+} || {
+  printf "\033[31mSource file not found.\nRun this script in \"build\" folder please.\033[0m\n";
+}
