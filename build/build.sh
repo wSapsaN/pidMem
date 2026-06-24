c5ddae15b8e1fa0c6a5757d7df3c9797047a658a
nikolay <n.koshelev@tightvideo.com>
2026-06-24 11:57:05 +1000

    builder

--- build/build.sh
+++ build/build.sh
@@ -15,14 +15,14 @@ FLAGS="-g -Wall -I${HEADERS_PATH} -o ${NAMEAPP}";
     FULL_SOURCE_PATH="${FULL_SOURCE_PATH} ${SOURCE_FOLDER}/${file_name}";
   done
   
-  # printf "%s\n" "${FULL_SOURCE_PATH}";
-
   BUILD="gcc${FULL_SOURCE_PATH} ${FLAGS}"
   printf "\033[34m%s\033[0m\n" "${BUILD}";
   
   eval "${BUILD}";
-
-  printf "\033[32;40mThe assembly is ready. Your file \"${NAMEAPP}\".\033[0m\n";
+  [[ $(echo "${?}") == 0 ]] && printf "\033[32;40mThe assembly is ready. Your file \"${NAMEAPP}\".\033[0m\n" || {
+    printf "\033[31mError Build\033[0m\n";
+  }
+  
 } || {
   printf "\033[31mSource file not found.\nRun this script in \"build\" folder please.\033[0m\n";
 }
